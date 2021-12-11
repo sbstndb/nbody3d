d9da40d96b70668643812a0587661219f696d881
sbstndbs <sebastiendubois4@gmail.com>
2021-12-11 02:41:18 +0100

    debug + finish intrinsic
    There are many debugs on arrays
    implementation of intrinsic is now working with great results

--- nbody3D/0/nbody3.c
+++ nbody3D/0/nbody3.c
@@ -19,7 +19,7 @@ typedef unsigned long long u64;
 //} particle_t;
 
 //
-void init(float *x, float *y, float *z, float *vx, float *vy, float *vz, *softening, softening_value, u64 n)
+void init(float *x, float *y, float *z, float *vx, float *vy, float *vz, float *softening, float softening_value, float *dt, float dt_value,  u64 n)
 {
 	for (u64 i = 0; i < n; i++)
 	{
@@ -38,18 +38,19 @@ void init(float *x, float *y, float *z, float *vx, float *vy, float *vz, *soften
 		vy[i] = sign * (f32)rand() / (f32)RAND_MAX;
 		vz[i] = (f32)rand() / (f32)RAND_MAX;
 	}
-	for (u64 i  = 0 < i < 64 ; i++){
+	for (u64 i  = 0  ; i < 64 ; i++){
 		softening[i] = softening_value;
+		dt[i] = dt_value ; 
 	}
 }
 
 //
-void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *vz, float *softening, float *time, float time_value, const f32 dt, u64 n)
+void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *vz, float *softening, float *dt, u64 n)
 {
 	//
-	const f32 softening_value = 1e-20;
 	__m256 rxi, ryi, rzi, rxj, ryj, rzj , rfx, rfy, rfz;
-	__m256 rdx  , rdy , rdz , rdxyz , tmp, tmp2, rsoft;
+	__m256 rvxi, rvyi, rvzi ; 
+	__m256 rdx  , rdy , rdz , rdxyz , tmp, tmp2, rsoft, rt;
 	rxi = _mm256_setzero_ps() ;
 	ryi = _mm256_setzero_ps() ;	
 	rzi = _mm256_setzero_ps() ;	
@@ -60,9 +61,7 @@ void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *v
 	rxj = _mm256_setzero_ps() ;
 	ryj = _mm256_setzero_ps() ;	
 	rzj = _mm256_setzero_ps() ;	
-	rvxj = _mm256_setzero_ps() ;	
-	rvyj = _mm256_setzero_ps() ;	
-	rvzj = _mm256_setzero_ps() ;	
+
 	
 	rdx = _mm256_setzero_ps() ;
 	rdy = _mm256_setzero_ps() ;	
@@ -71,7 +70,8 @@ void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *v
 	rdxyz = _mm256_setzero_ps() ;	
 	tmp = _mm256_setzero_ps() ;		
 	tmp2 = _mm256_setzero_ps() ;
-	rsoft = _mm256_load_ps(&softening[0]) ;	
+	rsoft = _mm256_load_ps(&softening[0]) ;
+	rt = _mm256_load_ps(&dt[0]) ;	
 	for (u64 i = 0; i < n; i++)
 	{
 	//
@@ -84,8 +84,9 @@ void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *v
 		ryi = _mm256_loadu_ps(&y[i]);
 		rzi = _mm256_loadu_ps(&z[i]);			
 		
-		for (u64 j = 0; j < n; j++)
+		for (u64 j = 0; j < n; j+=8)
 		{
+			//printf("j : %lld\n", j);
 			
 			rxj = _mm256_load_ps(&x[j]);
 			ryj = _mm256_load_ps(&y[j]);
@@ -99,7 +100,7 @@ void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *v
 			tmp =   _mm256_sub_ps(rdz, rdz) ;
 			rdxyz = _mm256_add_ps(tmp, rdxyz) ;	
 			rdxyz = _mm256_add_ps(rsoft, rdxyz) ;	
-			tpm = _mm256_sqrt_ps(rdxyz) ;
+			tmp = _mm256_sqrt_ps(rdxyz) ;
 			rdxyz = _mm256_mul_ps(tmp, tmp) ;	
 			rdxyz = _mm256_mul_ps(rdxyz, tmp) ;	
 			
@@ -107,24 +108,35 @@ void move_particles(float *x, float *y, float *z, float *vx, float *vy, float *v
 			rdy = _mm256_div_ps(rdy, rdxyz) ; 
 			rdz = _mm256_div_ps(rdz, rdxyz) ; 			
 			
-			rfx = _mm256_add(rfx, rdx) ; 	
-			rfy = _mm256_add(rfy, rdy) ; 	
-			rfz = _mm256_add(rfz, rdz) ; 				
-
+			rfx = _mm256_add_ps(rfx, rdx) ; 	
+			rfy = _mm256_add_ps(rfy, rdy) ; 	
+			rfz = _mm256_add_ps(rfz, rdz) ; 				
 		}
+		
+		rfx = _mm256_mul_ps(rfx, rt) ; 	
+		rfy = _mm256_mul_ps(rfy, rt) ; 	
+		rfz = _mm256_mul_ps(rfz, rt) ; 		
+		
+		rvxi = _mm256_loadu_ps(&vx[i]);
+		rvyi = _mm256_loadu_ps(&vy[i]);
+		rvzi = _mm256_loadu_ps(&vz[i]);
+		
+		rvxi = _mm256_add_ps(rvxi, rfx) ; 	
+		rvyi = _mm256_add_ps(rvyi, rfy) ; 
+		rvzi = _mm256_add_ps(rvzi, rfz) ; 
+		
+		_mm256_storeu_ps(&vx[i], rvxi) ;				
+		_mm256_storeu_ps(&vy[i], rvyi) ;	
+		_mm256_storeu_ps(&vz[i], rvzi) ;	
 
-		//
-		vx[i] += dt * fx; //19
-		vy[i] += dt * fy; //21
-		vz[i] += dt * fz; //23
 	}
 
 	//3 floating-point operations
 	for (u64 i = 0; i < n; i++)
 	{
-	x[i] += dt * vx[i];
-	y[i] += dt * vy[i];
-	z[i] += dt * vz[i];
+	x[i] += dt[0] * vx[i];
+	y[i] += dt[0] * vy[i];
+	z[i] += dt[0] * vz[i];
 	}
 }
 
@@ -134,8 +146,8 @@ int main(int argc, char **argv)
 	//
 	const u64 n = (argc > 1) ? atoll(argv[1]) : 16384;
 	const u64 steps= 10;
-	const f32 dt = 0.01;
-
+	const f32 dt_value = 0.01;
+	const f32 softening_value = 1e-20;
 	//
 	f64 rate = 0.0, drate = 0.0;
 
@@ -144,16 +156,16 @@ int main(int argc, char **argv)
 
 	//
 	//particle_t *p = malloc(sizeof(particle_t) * n);
-	float *x = malloc(sizeof(float) * n) ; 
-	float *y = malloc(sizeof(float) * n) ; 	
-	float *z = malloc(sizeof(float) * n) ; 	
-	float *vx = malloc(sizeof(float) * n) ; 	
-	float *vy = malloc(sizeof(float) * n) ; 	
-	float *vz = malloc(sizeof(float) * n) ; 	
-	float *softening = malloc(sizeof(float) * 64) ; 
-	float *time = malloc(sizeof(float) * 64) ; 	
+	float *x = aligned_alloc(64, sizeof(float) * n) ; 
+	float *y = aligned_alloc(64, sizeof(float) * n) ; 	
+	float *z = aligned_alloc(64, sizeof(float) * n) ; 	
+	float *vx = aligned_alloc(64, sizeof(float) * n) ; 	
+	float *vy = aligned_alloc(64, sizeof(float) * n) ; 	
+	float *vz = aligned_alloc(64, sizeof(float) * n) ; 	
+	float *softening = aligned_alloc(64, sizeof(float) * 64) ; 
+	float *dt = aligned_alloc(64, sizeof(float) * 64) ; 	
 	//
-	init(x, y,z, vx, vy, vz, *softening, softening_value, *time, time_value n);
+	init(x, y,z, vx, vy, vz, softening, softening_value, dt, dt_value,  n);
 
 	const u64 sx = 6 *  sizeof(double) * n;
 
@@ -168,7 +180,7 @@ int main(int argc, char **argv)
 	//Measure
 	const f64 start = omp_get_wtime();
 
-	move_particles(x, y, z, vx, vy, vz, softening, time, dt, n);
+	move_particles(x, y, z, vx, vy, vz, softening, dt, n);
 
 	const f64 end = omp_get_wtime();
 
